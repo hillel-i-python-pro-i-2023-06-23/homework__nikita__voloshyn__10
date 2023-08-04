ec2a0a46e86605e6c763c67120d3d777063ab627
Nikita Voloshyn <nikita.voloshyn.odessa@gmail.com>
2023-08-04 18:11:22 +0300

    Revert "12 homework done"
    This reverts commit e53c912ff605035a0d56cc3527c0183788a87cbc.

--- app/management/commands/delete_contacts.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from app.models import Contact
-
-
-class Command(BaseCommand):
-    help = "Delete all contacts"
-
-    def handle(self, *args, **kwargs):
-        Contact.objects.all().delete()
